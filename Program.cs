0793ac033572967f028dc7259c9a733568207b55
Fabrikpz <fabrisilveyra@gmail.com>
2023-11-03 20:59:58 -0300

    ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main()
+    {
+        List<int> numbers = new List<int>();
+        numbers.Add(1);
+        numbers.Add(2);
+        numbers.Add(3);
+        Stack<int> myStack = new Stack<int>();
+        foreach (var num in numbers)
+            myStack.Push(num);
+        while (myStack.Count > 0)
+        {
+            Console.WriteLine(myStack.Pop());
+        }
+    }
+}
